6b11d324cb651d0389a80465c64e9fdc600c80e9
Ilija <illradoicic@gmail.com>
2025-02-18 22:56:06 +0900

    add xendex

--- app/utils/programs.ts
+++ app/utils/programs.ts
@@ -95,6 +95,10 @@ export enum PROGRAM_NAMES {
 
     // Lighthouse
     LIGHTHOUSE_PROGRAM = 'Lighthouse Program',
+
+    //Xendex
+    XENDEX_AMM = 'Xendex Swap Program',
+    XENDEX_STAKING = 'Xendex Staking Program',
 }
 
 const ALL_CLUSTERS = [Cluster.Custom, Cluster.Devnet, Cluster.Testnet, Cluster.MainnetBeta];
@@ -464,6 +468,14 @@ export const PROGRAM_INFO_BY_ID: { [address: string]: ProgramInfo } = {
         deployments: [Cluster.MainnetBeta],
         name: PROGRAM_NAMES.WORMHOLE_TOKEN,
     },
+    7EEuq61z9VKdkUzj7G36xGd7ncyz8KBtUwAWVjypYQHf: {
+        deployments: [Cluster.MainnetBeta],
+        name: PROGRAM_NAMES.XENDEX_AMM,
+    },
+    E279H61mv8i4kc6P66HD8cSe4fApsEeZsf9rE1qxRPQc: {
+        deployments: [Cluster.MainnetBeta],
+        name: PROGRAM_NAMES.XENDEX_STAKING,
+    }
 };
 
 export const SPECIAL_IDS: { [key: string]: string } = {
