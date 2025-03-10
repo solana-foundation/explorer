d74a1ffe155bbf287bd3eba761388ea142018632
Sergo <rogaldh@radsh.red>
2025-03-10 17:27:15 +0000

    feat: wrap AnchorProgram card with boundary

--- app/components/inspector/InstructionsSection.tsx
+++ app/components/inspector/InstructionsSection.tsx
@@ -4,8 +4,10 @@ import { ASSOCIATED_TOKEN_PROGRAM_ID } from '@solana/spl-token';
 import { ComputeBudgetProgram, MessageCompiledInstruction, VersionedMessage } from '@solana/web3.js';
 import { getProgramName } from '@utils/tx';
 import React from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 
 import { useAnchorProgram } from '@/app/providers/anchor';
+import { getAnchorProgramName } from '@/app/utils/anchor';
 
 import AnchorDetailsCard from '../instruction/AnchorDetailsCard';
 import { ComputeBudgetDetailsCard } from '../instruction/ComputeBudgetDetailsCard';
@@ -41,23 +43,32 @@ function InspectorInstructionCard({
     const transactionInstruction = intoTransactionInstructionFromVersionedMessage(ix, message);
 
     if (anchorProgram.program) {
-        return AnchorDetailsCard({
-            anchorProgram: anchorProgram.program,
-            childIndex: undefined,
-            index: index,
-            // Inner cards and child are not used since we do not know what CPIs
-            // will be called until simulation happens, and even then, all we
-            // get is logs, not the TransactionInstructions
-            innerCards: undefined,
-            ix: transactionInstruction,
-            // Always display success since it is too complicated to determine
-            // based on the simulation and pass that result here. Could be added
-            // later if desired, possibly similar to innerCards from parsing tx
-            // sim logs.
-            result: { err: null },
-            // Signature is not needed.
-            signature: '',
-        });
+        const programName = getAnchorProgramName(anchorProgram.program) ?? 'Unknown Program';
+        return (
+            <ErrorBoundary
+                fallback={
+                    <UnknownDetailsCard key={index} index={index} ix={ix} message={message} programName={programName} />
+                }
+            >
+                <AnchorDetailsCard
+                    anchorProgram={anchorProgram.program}
+                    childIndex={undefined}
+                    index={index}
+                    // Inner cards and child are not used since we do not know what CPIs
+                    // will be called until simulation happens, and even then, all we
+                    // get is logs, not the TransactionInstructions
+                    innerCards={undefined}
+                    ix={transactionInstruction}
+                    // Always display success since it is too complicated to determine
+                    // based on the simulation and pass that result here. Could be added
+                    // later if desired, possibly similar to innerCards from parsing tx
+                    // sim logs.
+                    result={{ err: null }}
+                    // Signature is not needed.
+                    signature=""
+                />
+            </ErrorBoundary>
+        );
     }
 
     /// Handle program-specific cards here
