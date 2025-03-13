d8582f65421882336305d2a3a1a46491e8ae9d61
Noah Gundotra <noah@gundotra.org>
2025-03-13 14:58:07 -0400

    polyfill & migrate fetch-resource

--- test-setup.ts
+++ test-setup.ts
@@ -1,9 +1,16 @@
-import { expect } from 'vi';
 import '@testing-library/jest-dom';
 
 import { TextEncoder } from 'util';
 // Needed for @sqds/multisig
 global.TextEncoder = TextEncoder;
+
+if (!AbortSignal.timeout) {
+    AbortSignal.timeout = (ms) => {
+        const controller = new AbortController();
+        setTimeout(() => controller.abort(), ms);
+        return controller.signal;
+    };
+}
 
 // Needed for @solana/web3.js to treat Uint8Arrays as Buffers
 // See https://github.com/anza-xyz/solana-pay/issues/106
