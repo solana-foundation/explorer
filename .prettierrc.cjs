4edc7ba6cf9b98be8701f03f532694dbfacfd395
Sergo <rogaldh@radsh.red>
2025-03-28 17:35:26 +0000

    chore: improve prettier configuration

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,24 +1,6 @@
-const solanaPrettierConfig = require('@solana/prettier-config-solana');
+const prettierConfig = require('@solana/prettier-config-solana');
 
-/** @type {import("prettier").Config} */
 module.exports = {
-    ...solanaPrettierConfig,
+    ...prettierConfig,
     endOfLine: 'lf',
-    plugins: [
-        // prettier-plugin-tailwindcss will be loaded automatically by Prettier
-    ],
-    overrides: [
-        {
-            files: '*.{ts,tsx,mts,mjs}',
-            options: {
-                parser: 'typescript',
-            },
-        },
-        {
-            files: '*.{json,md}',
-            options: {
-                singleQuote: false,
-            },
-        },
-    ],
 };
