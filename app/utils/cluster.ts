4aecf009e5c4fb578d04b1cba8d1cb442f898f8a
Ayaz Abbas <lfcayaz@gmail.com>
2025-04-01 21:01:39 +0100

    set available clusters to testnet only

--- app/utils/cluster.ts
+++ app/utils/cluster.ts
@@ -11,7 +11,7 @@ export enum Cluster {
     Custom,
 }
 
-export const CLUSTERS = [Cluster.MainnetBeta, Cluster.Testnet, Cluster.Devnet, Cluster.Custom];
+export const CLUSTERS = [Cluster.Testnet, Cluster.Custom];
 
 export function clusterSlug(cluster: Cluster): string {
     switch (cluster) {
