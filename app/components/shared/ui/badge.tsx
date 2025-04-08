2e394c5f42f0c87d300cc0480d638fbac2037e8e
Sergo <rogaldh@radsh.red>
2025-04-08 19:50:27 +0100

    remove duplicated modifiers

--- app/components/shared/ui/badge.tsx
+++ app/components/shared/ui/badge.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 import { cn } from '@/app/components/shared/utils';
 
 const badgeVariants = cva(
-    'e-inline-flex e-items-center e-justify-center e-border e-border-neutral-200 e-px-2 e-py-0.5 e-font-medium e-w-fit e-whitespace-nowrap e-shrink-0 [&>svg]:e-size-3 e-gap-1 [&>svg]:e-pointer-events-none focus-visible:e-border-neutral-950 focus-visible:e-ring-neutral-950/50 focus-visible:e-ring-[3px] aria-invalid:e-ring-red-500/20 dark:aria-invalid:e-ring-red-500/40 aria-invalid:e-border-red-500 e-transition-[color,box-shadow] e-overflow-hidden dark:e-border-neutral-800 dark:focus-visible:e-border-neutral-300 dark:focus-visible:e-ring-neutral-300/50 dark:aria-invalid:e-ring-red-900/20 dark:dark:aria-invalid:e-ring-red-900/40 dark:aria-invalid:e-border-red-900',
+    'e-inline-flex e-items-center e-justify-center e-border e-border-neutral-200 e-px-2 e-py-0.5 e-font-medium e-w-fit e-whitespace-nowrap e-shrink-0 [&>svg]:e-size-3 e-gap-1 [&>svg]:e-pointer-events-none focus-visible:e-border-neutral-950 focus-visible:e-ring-neutral-950/50 focus-visible:e-ring-[3px] aria-invalid:e-ring-red-500/20 dark:aria-invalid:e-ring-red-500/40 aria-invalid:e-border-red-500 e-transition-[color,box-shadow] e-overflow-hidden dark:e-border-neutral-800 dark:focus-visible:e-border-neutral-300 dark:focus-visible:e-ring-neutral-300/50 dark:aria-invalid:e-ring-red-900/20 dark:aria-invalid:e-ring-red-900/40 dark:aria-invalid:e-border-red-900',
     {
         compoundVariants: [
             {
@@ -54,7 +54,7 @@ const badgeVariants = cva(
                 default:
                     'e-border-transparent e-text-neutral-900 [a&]:hover:e-bg-neutral-100/90 dark:e-text-neutral-50 dark:[a&]:hover:e-bg-neutral-800/90',
                 destructive:
-                    'e-border-transparent e-bg-red-500 e-text-white [a&]:hover:e-bg-red-500/90 focus-visible:e-ring-red-500/20 dark:focus-visible:e-ring-red-500/40 dark:e-bg-red-500/60 dark:e-bg-red-900 dark:[a&]:hover:e-bg-red-900/90 dark:focus-visible:e-ring-red-900/20 dark:dark:focus-visible:e-ring-red-900/40 dark:dark:e-bg-red-900/60',
+                    'e-border-transparent e-bg-red-500 e-text-white [a&]:hover:e-bg-red-500/90 focus-visible:e-ring-red-500/20 dark:focus-visible:e-ring-red-500/40 dark:e-bg-red-500/60 dark:e-bg-red-900 dark:[a&]:hover:e-bg-red-900/90 dark:focus-visible:e-ring-red-900/20 dark:focus-visible:e-ring-red-900/40 dark:e-bg-red-900/60',
                 outline:
                     'e-text-neutral-950 [a&]:hover:e-bg-neutral-100 [a&]:hover:e-text-neutral-900 dark:e-text-neutral-50 dark:[a&]:hover:e-bg-neutral-800 dark:[a&]:hover:e-text-neutral-50',
                 passive:
