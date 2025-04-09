d04f2ad10bfdc82b11670afb46b03a31c7b19741
Sergo <rogaldh@radsh.red>
2025-04-09 17:24:16 +0100

    chore: improve config for prettier
    chore: improve config for prettier
    
    format existing code with new preset
    
    format existing code with new preset
    
    apply updated configuration for the project
    
    chore: add ci-specific script to check code-style upon changes at CI
    
    check prettier issue
    
    chore: improve config for prettier
    
    format existing code with new preset
    
    format existing code with new preset
    
    apply updated configuration for the project
    
    chore: add ci-specific script to check code-style upon changes at CI
    
    check prettier issue
    
    fix

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,5 +1,6 @@
 const prettierConfigSolana = require('@solana/prettier-config-solana');
 
+/** @type {import("prettier").Config} */
 module.exports = {
     ...prettierConfigSolana,
     plugins: [prettierConfigSolana.plugins ?? []].concat(['prettier-plugin-tailwindcss']),
