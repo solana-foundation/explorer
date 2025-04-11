c0032d3d15a17b7bbc0b5e3132888e415075e1b1
Sergo <rogaldh@radsh.red>
2025-04-11 16:25:24 +0100

    remove log

--- app/__tests__/mocks.ts
+++ app/__tests__/mocks.ts
@@ -93,6 +93,5 @@ export function deserializeInstruction(instruction: string): MessageCompiledInst
 
 export async function sleep(ms?: number): Promise<void> {
     const timeoutMs = ms || (process.env.TEST_SERIAL_TIMEOUT ? Number(process.env.TEST_SERIAL_TIMEOUT.trim()) : 1500);
-    console.log({ timeoutMs });
     return await new Promise(resolve => setTimeout(resolve, timeoutMs));
 }
