2893ea5fb0040b5910332dfc73ec8d1454c83e10
Noah Gundotra <noah@gundotra.org>
2025-04-11 17:46:00 -0400

    add ecosystem cards

--- app/page.tsx
+++ app/page.tsx
@@ -40,11 +40,86 @@ export default function Page() {
                             <LiveTransactionStatsCard />
                         </div>
                     </div>
+                    <div className="card">
+                        <div className="card-body">
+                            <div className="card-title d-flex justify-content-between border-bottom border-gray-300 pb-2">
+                                <div className="me-4">Kickstart your development journey on Solana</div>
+                                <div>
+                                    Find more on{' '}
+                                    <a href="https://solana.com/developers" target="_blank" rel="noreferrer">
+                                        solana.com/developers
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="d-flex gap-4 pb-3 overflow-auto">
+                                <EcosystemCard
+                                    title="Solana Developer MCP"
+                                    description="AI powered Solana developer tools for Cursor, Windsurf, Cline and more"
+                                    image="https://mcp.solana.com/meta.png"
+                                    link="https://mcp.solana.com"
+                                />
+                                <EcosystemCard
+                                    title="brine-ed25519"
+                                    description="A pure-Rust implementation of Ed25519 digital signatures"
+                                    image="https://private-user-images.githubusercontent.com/623790/432522277-cc354cf3-b82d-40c6-8c9a-9902fae146f0.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDQ0MDc5ODksIm5iZiI6MTc0NDQwNzY4OSwicGF0aCI6Ii82MjM3OTAvNDMyNTIyMjc3LWNjMzU0Y2YzLWI4MmQtNDBjNi04YzlhLTk5MDJmYWUxNDZmMC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwNDExJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDQxMVQyMTQxMjlaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0xN2YwMDA0NjI0MTM2YWJhNGIzZGZhNjZhODY4ZmNkOThlM2MzZjEyZDk0NWMyODNhNGJjN2Y4ZGMyMmYzYjQxJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.k1TiwzVzO-cEeZl99Re-izipt_P2GXWJ0U6zF2_bb3s"
+                                    link="https://github.com/zfedoran/brine-ed25519"
+                                />
+                                <EcosystemCard
+                                    title="Gill"
+                                    description="A Simplified Javascript Client for Solana"
+                                    image="https://raw.githubusercontent.com/solana-foundation/gill/refs/heads/master/media/cover.png"
+                                    link="https://github.com/solana-foundation/gill"
+                                />
+                                <EcosystemCard
+                                    title="Pinocchio"
+                                    description="A Solana CLI tool for creating and managing Solana programs"
+                                    image="https://private-user-images.githubusercontent.com/729235/355061608-3a1894b4-403f-4c35-90aa-548e7672fe90.jpg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDQ0MDc5NDMsIm5iZiI6MTc0NDQwNzY0MywicGF0aCI6Ii83MjkyMzUvMzU1MDYxNjA4LTNhMTg5NGI0LTQwM2YtNGMzNS05MGFhLTU0OGU3NjcyZmU5MC5qcGc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwNDExJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDQxMVQyMTQwNDNaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1iOGVlMzcxYWZkMTVmZDM3ZmQwOTIyOTA0ZDc5YTliYmIwMTg3Y2MxZWM5ZTA4YTIzMTYxOTZlODY3M2E3YjI0JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.A-knzozh3jI3qe17grkmg6scHrTJAAk4ellDlMAMAU0"
+                                    link="https://github.com/anza-xyz/pinocchio"
+                                />
+                                <EcosystemCard
+                                    title="Codama"
+                                    description="A Solana CLI tool for creating and managing Solana programs"
+                                    image="https://private-user-images.githubusercontent.com/3642397/374996178-029af336-ea71-4e7f-9612-ef5bb187e3a0.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDQ0MDgwNDMsIm5iZiI6MTc0NDQwNzc0MywicGF0aCI6Ii8zNjQyMzk3LzM3NDk5NjE3OC0wMjlhZjMzNi1lYTcxLTRlN2YtOTYxMi1lZjViYjE4N2UzYTAucG5nP1gtQW16LUFsZ29yaXRobT1BV1M0LUhNQUMtU0hBMjU2JlgtQW16LUNyZWRlbnRpYWw9QUtJQVZDT0RZTFNBNTNQUUs0WkElMkYyMDI1MDQxMSUyRnVzLWVhc3QtMSUyRnMzJTJGYXdzNF9yZXF1ZXN0JlgtQW16LURhdGU9MjAyNTA0MTFUMjE0MjIzWiZYLUFtei1FeHBpcmVzPTMwMCZYLUFtei1TaWduYXR1cmU9ZjJkN2IzMDA0NzVhNTk1MjU4NDY0MjcwM2U0Mzg4MTQ4ZDVmOWU2ZGFkNTc3MzBjZjJiOWIxZjQ1MGY2ZTFjYSZYLUFtei1TaWduZWRIZWFkZXJzPWhvc3QifQ.5lEayJzf-0ZIxiMz8RbhMaUaluZPym5dFGy7blhlnMw"
+                                    link="https://github.com/codama-idl/codama"
+                                />
+                            </div>
+                        </div>
+                    </div>
 
                     <UpcomingFeatures />
                 </div>
             </SupplyProvider>
         </StatsProvider>
+    );
+}
+
+function EcosystemCard({
+    title,
+    description,
+    image,
+    link,
+}: {
+    title: string;
+    description: string;
+    image: string;
+    link: string;
+}) {
+    return (
+        <div className="flex flex-col" style={{ width: '250px', height: '200px' }}>
+            <div className="w-full mb-3">
+                <a href={link} target="_blank" rel="noopener noreferrer" className="hover:cursor-pointer">
+                    <img
+                        src={image}
+                        alt={`${title} preview`}
+                        style={{ width: '250px', height: '120px', objectFit: 'cover' }}
+                    />
+                </a>
+            </div>
+            <div className="flex flex-col">
+                <p className="mb-1">{title}</p>
+                <p className="text-muted mb-2 text-wrap line-clamp-3">{description}</p>
+            </div>
+        </div>
     );
 }
 
