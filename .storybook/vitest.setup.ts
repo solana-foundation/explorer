9c41f04b010105efaf46fe5a4b16f8f06e87ecdc
Sergo <rogaldh@radsh.red>
2025-04-25 15:41:00 +0100

    fix

--- .storybook/vitest.setup.ts
+++ .storybook/vitest.setup.ts
@@ -2,16 +2,10 @@ import { beforeAll, vi } from 'vitest';
 import { setProjectAnnotations } from '@storybook/experimental-nextjs-vite';
 import * as projectAnnotations from './preview';
 
-// vi.mock('child_process', () => ({
-//     spawn: () => ({
-//         stdout: { on: (_: string, cb: (data: Buffer) => void) => cb(Buffer.from('')) },
-//         on: (_: string, cb: () => void) => cb(),
-//     }),
-//     spawnSync: () => ({ stdout: Buffer.from(''), status: 0 }),
-// }));
-
 // This is an important step to apply the right configuration when testing your stories.
 // More info at: https://storybook.js.org/docs/api/portable-stories/portable-stories-vitest#setprojectannotations
 const project = setProjectAnnotations([projectAnnotations]);
+
+console.log({ project });
 
 beforeAll(project.beforeAll);
