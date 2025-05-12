a4d4768c56a309f40524c6a8c53609230299fc84
Sergo <rogaldh@radsh.red>
2025-05-12 19:47:13 +0100

    fix

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -5,19 +5,4 @@ module.exports = {
     ...prettierConfigSolana,
     plugins: [prettierConfigSolana.plugins ?? []].concat(['prettier-plugin-tailwindcss']),
     endOfLine: 'lf',
-    overrides: [
-        ...(prettierConfigSolana.overrides ?? []),
-        {
-            files: '*.{ts,tsx,mts,mjs}',
-            options: {
-                parser: 'typescript',
-            },
-        },
-        {
-            files: '*.{json,md}',
-            options: {
-                singleQuote: false,
-            },
-        },
-    ],
 };
