ca419855229fb26d464498c2c437b736a30d80e9
Sergo <rogaldh@radsh.red>
2025-05-12 20:09:17 +0100

    recover needed lines

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -5,4 +5,19 @@ module.exports = {
     ...prettierConfigSolana,
     plugins: [prettierConfigSolana.plugins ?? []].concat(['prettier-plugin-tailwindcss']),
     endOfLine: 'lf',
+    overrides: [
+        ...(prettierConfigSolana.overrides ?? []),
+        {
+            files: '*.{ts,tsx,mts,mjs}',
+            options: {
+                parser: 'typescript',
+            },
+        },
+        {
+            files: '*.{json,md}',
+            options: {
+                singleQuote: false,
+            },
+        },
+    ],
 };
