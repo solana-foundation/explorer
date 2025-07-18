5a91ff87ac3f56391139ba22733a407cff46657d
Andrey Lizunov <andrey.tver69@gmail.com>
2025-07-18 21:00:48 +0300

    Fixed PR review remarks

--- app/utils/attestation-service.tsx
+++ app/utils/attestation-service.tsx
@@ -1,6 +1,5 @@
 import { Account } from '@providers/accounts';
-import { deserialize } from 'borsh';
-import { deserialize as deserializeWithVersion200 } from 'borsh2';
+import { deserialize } from 'borsh2';
 import {
     convertSasSchemaToBorshSchema,
     decodeAttestation,
@@ -58,58 +57,11 @@ export function isAttestationAccount(account: Account) {
     }
 }
 
-/**
-    Helper function to deserialize attestation data using the borsh@0.7.0 API
-    This is annoying because sas-lib relies on @solana/kit which uses borsh@2.0.0 but metaplex & other libraries use borsh@0.7.0
-    This is a workaround to get the data deserialized correctly.
-
-    TODO: remove implementation upon refactoring with major or minor change
-
-    @deprecated - legacy deserialization should not be used
-*/
-export function deserializeAttestationDataWithBorsh070(schema: SasSchema, data: Uint8Array) {
-    // 1. Use sas-lib to convert its schema into a borsh-compatible format.
-    const sasBorshSchema = convertSasSchemaToBorshSchema(schema);
-    // @ts-expect-error borsh@0.7.0 actually exposes private schema field, even though it's not typed
-    const fields = Object.entries(sasBorshSchema.schema.struct);
-
-    // 2. We must dynamically create a class that has the properties defined in the schema.
-    const DynamicAttestationData = class {
-        constructor(properties: any) {
-            Object.keys(properties).forEach(key => {
-                (this as any)[key] = properties[key];
-            });
-        }
-    };
-
-    // Add properties to the class prototype from the schema fields
-    fields.forEach(([fieldName]) => {
-        Object.defineProperty(DynamicAttestationData.prototype, fieldName, {
-            enumerable: true,
-            writable: true,
-        });
-    });
-
-    // 3. The borsh@0.7.0 deserialize function requires a Map object
-    // where the key is the class and the value is the schema definition.
-    const schemaMap = new Map<any, any>();
-    const borsh070Schema = {
-        fields: fields,
-        kind: 'struct',
-    };
-    schemaMap.set(DynamicAttestationData, borsh070Schema);
-
-    // 4. Call borsh@0.7.0's deserialize function with the correct arguments.
-    // It will return an instance of DynamicAttestationData with populated fields.
-    return deserialize(schemaMap, DynamicAttestationData, Buffer.from(data));
-}
-
 // Helper function to deserialize attestation data using the borsh@2.0.0 API
 export function deserializeAttestationDataWithBorsh200(schema: SasSchema, data: Uint8Array) {
     // Use sas-lib to convert its schema into a borsh-compatible format.
     const sasBorshSchema = convertSasSchemaToBorshSchema(schema);
 
     // @ts-expect-error borsh@2.0.0 actually exposes private schema field, even though it's not typed
-    const deserializedData = deserializeWithVersion200(sasBorshSchema.schema, data);
-    return deserializedData;
+    return deserialize(sasBorshSchema.schema, data);
 }
