3c2be27f480e58de4bdf52f9a24b6d36bb548070
Sergo <rogaldh@radsh.red>
2025-09-09 16:40:57 +0100

    feat: show statistics for additional programs

--- app/api/shared/constants.ts
+++ app/api/shared/constants.ts
@@ -1,3 +1,9 @@
+import { PROGRAM_METADATA_PROGRAM_ADDRESS } from '@solana-program/program-metadata';
 import { TOKEN_2022_PROGRAM_ADDRESS } from '@solana-program/token-2022';
+import { SOLANA_ATTESTATION_SERVICE_PROGRAM_ADDRESS } from 'sas-lib';
 
-export const ENABLED_PROGRAMS_FOR_CPI_CALLS = [TOKEN_2022_PROGRAM_ADDRESS.toString()];
+export const ENABLED_PROGRAMS_FOR_CPI_CALLS = [
+    TOKEN_2022_PROGRAM_ADDRESS.toString(),
+    SOLANA_ATTESTATION_SERVICE_PROGRAM_ADDRESS.toString(),
+    PROGRAM_METADATA_PROGRAM_ADDRESS.toString(),
+];
