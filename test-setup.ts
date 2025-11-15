c7067257e404bff888fab66bdc7525f6f6b75812
ewa <elvestr+github@gmail.com>
2025-11-15 17:34:25 +0300

    Update test-setup.ts
    update

--- test-setup.ts
+++ test-setup.ts
@@ -1,18 +1,25 @@
 import '@testing-library/jest-dom';
 
-if (!AbortSignal.timeout) {
-    AbortSignal.timeout = ms => {
-        const controller = new AbortController();
-        setTimeout(() => controller.abort(), ms);
-        return controller.signal;
-    };
+// -----------------------------
+// AbortSignal.timeout polyfill
+// -----------------------------
+if (!(AbortSignal as any).timeout) {
+  (AbortSignal as any).timeout = (ms: number) => {
+    const controller = new AbortController();
+    setTimeout(() => controller.abort(), ms);
+    return controller.signal;
+  };
 }
 
-// Needed for @solana/web3.js to treat Uint8Arrays as Buffers
-// See https://github.com/anza-xyz/solana-pay/issues/106
+// ---------------------------------------------
+// Solana Web3.js: Uint8Array/Buffer uyumluluğu
+// ---------------------------------------------
 const originalHasInstance = Uint8Array[Symbol.hasInstance];
+
 Object.defineProperty(Uint8Array, Symbol.hasInstance, {
-    value(potentialInstance: any) {
-        return originalHasInstance.call(this, potentialInstance) || Buffer.isBuffer(potentialInstance);
-    },
+  value(potentialInstance: any) {
+    return originalHasInstance.call(this, potentialInstance) || Buffer.isBuffer(potentialInstance);
+  },
+  configurable: true,
+  writable: true,
 });
