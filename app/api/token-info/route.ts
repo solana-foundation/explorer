2f21cd8d2b1baef0fa0209118e6c25575a26eca8
Sergo <rogaldh@radsh.red>
2026-02-03 22:28:15 +0000

    remove cache-headers as we use POST

--- app/api/token-info/route.ts
+++ app/api/token-info/route.ts
@@ -3,10 +3,6 @@ import { Cluster } from '@utils/cluster';
 import { NextResponse } from 'next/server';
 
 const CACHE_MAX_AGE = 3600; // 1 hour
-const CACHE_SWR = 86400; // 24 hours - serve stale while revalidating
-const CACHE_HEADERS = {
-    'Cache-Control': `public, max-age=${CACHE_MAX_AGE}, s-maxage=${CACHE_MAX_AGE}, stale-while-revalidate=${CACHE_SWR}`,
-};
 
 export async function POST(request: Request) {
     const { address, cluster } = await request.json();
@@ -18,5 +14,5 @@ export async function POST(request: Request) {
     const tokens = await getTokenInfos([address], cluster as Cluster, {
         next: { revalidate: CACHE_MAX_AGE },
     });
-    return NextResponse.json({ content: tokens[0] }, { headers: CACHE_HEADERS });
+    return NextResponse.json({ content: tokens[0] });
 }
