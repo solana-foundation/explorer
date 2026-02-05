5a85302d21381e08bcf4e8cc5c8e912a2c2c8905
Alex S <alexander.shibaev@hoodies.team>
2026-02-05 13:33:48 +0700

    chore: enable edge runtime for receipt route

--- app/og/receipt/[signature]/route.tsx
+++ app/og/receipt/[signature]/route.tsx
@@ -12,7 +12,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 import Logger from '@/app/utils/logger';
 
-// export const runtime = 'edge';
+export const runtime = 'edge';
 
 const CACHE_DURATION = 30 * 60; // 30 minutes
 const DEFAULT_CACHE_HEADERS = {
