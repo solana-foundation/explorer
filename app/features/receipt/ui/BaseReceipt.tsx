6a657821459a809c32bcbceabf8d473f8fd220eb
Alex S <alexander.shibaev@hoodies.team>
2026-02-27 16:08:21 +0700

    fix(receipt): Prevent text overflow in total display by adding whitespace nowrap class

--- app/features/receipt/ui/BaseReceipt.tsx
+++ app/features/receipt/ui/BaseReceipt.tsx
@@ -193,7 +193,7 @@ function Footer({
                                         className="e-flex-shrink-0"
                                     />
                                 ))}
-                            <span className="e-text-2xl e-text-white">
+                            <span className="e-text-2xl e-text-white e-whitespace-nowrap">
                                 {total.formatted} {total.unit}
                             </span>
                         </div>
