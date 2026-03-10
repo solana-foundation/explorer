b2c6388f5b6c5130efbcdbb07440491c0cef44a1
Sergo <rogaldh@radsh.red>
2026-03-10 14:09:28 +0000

    chore: optimize icon with svgo

--- app/features/receipt/ui/icons/XIcon.tsx
+++ app/features/receipt/ui/icons/XIcon.tsx
@@ -1,9 +1,9 @@
 export function XIcon() {
     return (
-        <svg fill="none" height="11" viewBox="0 0 11 11" width="11" xmlns="http://www.w3.org/2000/svg">
+        <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" fill="none" viewBox="0 0 11 11">
             <path
-                d="M8.65 0H10.3L6.695 4.14L10.945 11H7.64L5.08 7.155L2.15 11H0.49L4.355 6.57L0.28 0H3.665L5.99 3.53L8.65 0ZM8.075 9.88H8.99L3.19 0.94H2.21L8.075 9.88Z"
                 fill="currentColor"
+                d="M8.65 0h1.65L6.695 4.14l4.25 6.86H7.64L5.08 7.155 2.15 11H.49l3.865-4.43L.28 0h3.385L5.99 3.53zm-.575 9.88h.915L3.19.94h-.98z"
             />
         </svg>
     );
