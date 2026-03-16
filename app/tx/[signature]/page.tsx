14a7001336053c592feaccf90a658f38fb7b96aa
Tania Markina <tanyamarkina@list.ru>
2026-03-16 16:49:53 +0100

    chore: build info

--- app/tx/[signature]/page.tsx
+++ app/tx/[signature]/page.tsx
@@ -1,8 +1,8 @@
 import '../../styles.css';
 
-import { buildCompositeSignature } from '@features/receipt/server';
 import { getClusterParam } from '@features/receipt';
 import { isReceiptEnabled, RECEIPT_BASE_URL, RECEIPT_OG_IMAGE_VERSION } from '@features/receipt/env';
+import { buildCompositeSignature } from '@features/receipt/server';
 import { Cluster, CLUSTERS, clusterSlug } from '@utils/cluster';
 import { SignatureProps } from '@utils/index';
 import { Metadata } from 'next/types';
