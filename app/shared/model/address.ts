e946e18f113afb2b561151f41269dcb12cf82dc5
Sergo <rogaldh@radsh.red>
2026-04-08 18:45:20 +0100

    fix: codestyle

--- app/shared/model/address.ts
+++ app/shared/model/address.ts
@@ -1,4 +1,4 @@
-import { address, type Address } from '@solana/kit';
+import { type Address, address } from '@solana/kit';
 import type { PublicKey } from '@solana/web3.js';
 
 export function toAddress(publicKey: PublicKey): Address {
