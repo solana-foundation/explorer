f38b12b6839a8cc4fd458bf776fb525a6a928858
Sergo <rogaldh@radsh.red>
2026-06-03 20:35:27 +0100

    refactor(dashkit): swap text-muted for e-text-dk-gray-700 in BlockhashesCard

--- app/components/account/BlockhashesCard.tsx
+++ app/components/account/BlockhashesCard.tsx
@@ -35,7 +35,7 @@ export function BlockhashesCard({ blockhashes }: { blockhashes: RecentBlockhashe
                 </div>
 
                 <CardFooter ui="dashkit">
-                    <div className="text-muted e-text-center">
+                    <div className="e-text-dk-gray-700 e-text-center">
                         {blockhashes.length > 0 ? '' : 'No blockhashes found'}
                     </div>
                 </CardFooter>
